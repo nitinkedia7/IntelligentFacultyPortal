337fb4e9691a77406b0e1b8985cff6acdc96bfd8
Nitin Kedia <nitinkedia7@gmail.com>
2017-11-14 00:31:23 +0530

    Add basic html templates

--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Department, Designation, Faculty, Education, Course, Journal, Conference, ResearchInterest, ProfessionalExperience, Achievement, AdministrativeResponsibilitie
 # Create your views here.
+
+def index(request):
+    return render(
+        request,
+        'index.html',
+    )
